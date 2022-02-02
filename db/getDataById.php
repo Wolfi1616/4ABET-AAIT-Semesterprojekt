1348aadfcf0cf7838ba5ec840e7d5406527fdd70
Wolfi1616 <wolfgang@web-craft.design>
2022-02-02 15:03:23 +0100

    .

---
+++ db/getDataById.php
@@ -0,0 +1,37 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
+$id = isset($_POST['id']) ? $_POST['id'] : exit('0');
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'kinderbeobachtungen';
+
+$mysqli = new mysqli($servername, $username, $password, $database);
+
+if ($mysqli->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
+}
+$sql = '
+SELECT
+ *
+FROM daten
+WHERE
+    daten.id = "'.$id.'"
+';
+
+if ($result = $mysqli->query($sql)) {
+
+    $json = json_encode($result->fetch_assoc());		
+} else {
+    errorMsg('SQL-Fehler '.$mysqli->errno.': '.$mysqli->error);
+}
+
+$mysqli->query($sql);
+$mysqli->close();
+
+echo $json;
--- js/script.js
+++ js/script.js
@@ -3,22 +3,19 @@ $('section').css('display', 'none');
 $('section').first().css('display', 'block');
 
 var text = $('ul').html();
-$('section > h2').each(function () {
-    text += '<li ng-click="menu(\'' + $(this).text() + '\')">' + $(this).text() + '</li>';
+$('section').each(function () {
+    text += '<li ng-click="routerNavigation(\'' + $(this).attr('menuName') + '\')">' + $(this).attr('menuName') + '</li>';
 });
 $('ul').html(text);
-
 
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $filter, $http) {
 
-    //DEBUG-FUNCTIONS
+    //DEBUG
     $scope.debug = function() {
         zeitdifferenzBerechnen($('#aufstehzeit').val(), $('#schlafzeit').val());
     }
-
-
 
     //DEKLARATIONEN
 
@@ -29,20 +26,21 @@ app.controller('myCtrl', function($scope, $filter, $http) {
     $scope.datum = '';
     $scope.kind = '';
 
-
-    //CRUD-OPERATOREN:
-    $scope.update = false;
-
-
-    $scope.mehrData = [];
+    //DIVERS
+    $scope.data = [];
     $scope.suche = '';
+    $scope.formTitle = 'Daten hinzufügen';
+    $scope.updateCondition = false;
+
+
 
     // NAV-MENU TOGGLES
-    $scope.menu = function(activeMenu) {
+    $scope.routerNavigation = function(activeMenu) {
+  //  function routerNavigation(activeMenu) {
        $('section').css('display', 'none');
-       $('section > h2').each(function() {
-           if (activeMenu == $(this).text() ) {
-               $(this).parent().css('display', 'block');
+       $('section').each(function() {
+           if (activeMenu == $(this).attr('menuName') ) {
+               $(this).css('display', 'block');
            }
        });
     };
@@ -63,7 +61,9 @@ app.controller('myCtrl', function($scope, $filter, $http) {
         return stunden + ':' + minuten;
     }
 
-    function updateInputValues(type) {
+
+
+    function updateInputValues(type, id) {
         if (type == 'create') {
             $scope.aufstehzeit = $('#aufstehzeit').val();
             $scope.schlafzeit = $('#schlafzeit').val();
@@ -71,7 +71,29 @@ app.controller('myCtrl', function($scope, $filter, $http) {
             $scope.kind = $('#name').val();
             $scope.wachzeit = zeitdifferenzBerechnen($scope.aufstehzeit, $scope.schlafzeit);
         } else if (type == 'update') {
-            console.log('Update');
+
+            console.log('Update ID: ' + id);
+            $http.post(
+                "db/getDataById.php", {
+                    id : id,
+                })
+            .then(function(response) {
+              console.log(response.data);
+              $scope.formTitle = 'Daten aktualisieren';
+              $('#aufstehzeit').val(response.data.aufstehzeit);
+              $('#schlafzeit').val(response.data.schlafzeit);
+              $('#datum').val(response.data.datum);
+              $('#name').val(response.data.kind);
+              //tmpAufstehzeit = response.data.aufstehzeit;
+              //$('#schlafzeit').val() = data.schlafzeit;
+
+            });            
+
+        } else if (type = 'clear') {
+            $('#aufstehzeit').val('');
+            $('#schlafzeit').val('');
+            $('#datum').val('');
+            $('#name').val('');
         }
 
     }
@@ -102,106 +124,36 @@ app.controller('myCtrl', function($scope, $filter, $http) {
     $scope.read = function() {
         $http.get("db/query.php")
         .then(function(response) {
-        $scope.mehrData = response.data;
+        $scope.data = response.data;
         });
     }
 
+    //EDIT (VORSTUFE ZU UPDATE ODER DELETE - BEFÜLLT DIE DATEN):
+    $scope.edit = function(id) {
+        $scope.updateCondition = true;
+        $scope.routerNavigation('Dateneingabe');
+        updateInputValues('update', id);
+    }   
 
     //UPDATE
     $scope.update = function() {
-        updateInputValues('update');
+
+        //UPDATE FUNKTION FEHLT NOCH!
+        $scope.redirectAndClear();
     }
     //DELETE
+    $scope.delete = function() {
 
-
-
-
-/*
-    // ALLE OBJEKTE BEIM SEITENAUFRUF LADEN:
-    $(document).ready(function() {
-        $scope.query();
-        //console.log($scope.mehrData);
-       // setTimeout($scope.query, 3500);
-    });
-
-
-
-    //AJAX
-    //DATENBANKABFRAGE:
-    $scope.query = function() {
-        $.ajax({
-            url: 'db/query.php',
-            type: 'POST',
-            success: function(response) {
-               // console.log("Success");
-               // console.log(response);
-               $scope.mehrData = JSON.parse(response);
-               console.log($scope.mehrData);
-              // return JSON.parse(response);
-
-            },
-            error: function(response) {
-                console.log("Error");
-                console.log(response);
-            }
-        });    
+        // DELETE FUNKTION FEHLT NOCH!
+        $scope.redirectAndClear();
     }
-*/
+
+    $scope.redirectAndClear = function() {
+        $scope.formTitle = 'Daten hinzufügen';
+        $scope.updateCondition = false;
+        $scope.routerNavigation('Statistik');
+        updateInputValues('clear');
+    }
 
 
-/*
-    $scope.insert = function() {
-
-        // INPUT-VALIDIERUNG: Überprüft ob die [required] inputs ausgefüllt wurden!
-        var valid = true;
-
-        $('[required]').each(function() {
-            if( $(this).is(':invalid') ) valid = false;
-        });
-        if (!valid) {
-            alert('Bitte fülle alle Formularfelder aus!');
-            return;
-        };
-
-
-        // Wachzeit errechnen
-        $scope.inputData.wachzeit = parseFloat($scope.inputData.schlafzeit) - parseFloat($scope.inputData.aufstehzeit);
-        // Datum umrechnen bzw. ins richtige Format für die DB kriegen!
-        $scope.inputData.datum = $filter('date')($scope.inputData.datum, 'yyyy-MM-dd'),
-
-        $.ajax({
-            url: 'db/insert.php',
-            type: 'POST',
-            data: {
-                aufstehzeit : parseFloat($scope.inputData.aufstehzeit),
-                schlafzeit : parseFloat($scope.inputData.schlafzeit),
-                wachzeit : parseFloat($scope.inputData.wachzeit),
-                name : $scope.inputData.name,
-                datum : $scope.inputData.datum,
-            }, 
-            success: function(response) {
-                console.log("Success");
-                //console.log(response);
-                //console.log($scope.mehrData);
-                //Temporäre Inputdaten zurücksetzen!
-                $scope.inputData = {
-                    'datum' : new Date(),
-                    'name' : '',
-                    'aufstehzeit' : '',
-                    'schlafzeit' : '',
-                    'wachzeit' : '',
-                };
-                console.log($scope.inputData);
-
-                $scope.query();
-
-            },
-            error: function(response) {
-                console.log("Error");
-                console.log(response);
-            }
-        });
-    };
-
-    */
 });
